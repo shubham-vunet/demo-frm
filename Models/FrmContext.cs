a1590fbd10327e0c6bc915f918d7e0b63d7a8f7d
Shubham Sharma <10323297+shubham-vunet@users.noreply.github.com>
2023-11-27 22:34:53 +0530

    WIP

---
+++ Models/FrmContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace frm.Models;
+
+public partial class FrmContext : DbContext
+{
+    public FrmContext(DbContextOptions<FrmContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
